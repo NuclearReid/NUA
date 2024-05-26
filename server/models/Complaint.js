9d944e7812de86a4530d9f0f58ff20f646f065ea
Reid Back <reid.backcnmt@gmail.com>
2024-05-26 21:45:31 +1000

    working at the complaint form. made the schema

---
+++ server/models/Complaint.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const complaintSchema = new Schema({
+
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    phoneNumber: {
+        type: Number,
+    },
+    dateOfCall: {
+        type: Date,
+    },
+    timeOfCall: {
+        type: String
+    },
+    natureOfGrievance: {
+        type: String
+    },
+    namesOfInvolved: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    impact: {
+        type: String
+    },
+    suggestions: {
+        type: String
+    },
+    confidentiality: {
+        type: String
+    }
+});
+
+const Complaint = model('Complaint', complaintSchema);
+
+module.exports = Complaint;
