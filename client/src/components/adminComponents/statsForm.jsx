357f5fecc6546540d2a5396c97e7e7a7227a017f
Reid Back <reid.backcnmt@gmail.com>
2024-05-29 09:44:35 +1000

    made a new statsForm component that goes in the Admin portal, and the default values in the form are the current values from the db. makes it easier to tick up with each call

---
+++ client/src/components/adminComponents/statsForm.jsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from 'react';
+import { SET_STATS } from '../../utils/mutations';
+import { QUERY_ADMIN } from '../../utils/queries';
+
+import { useMutation, useQuery } from '@apollo/client';
+
+export default function changeStatsForm() {
+    const {loading, data} = useQuery(QUERY_ADMIN);
+
+
+    const [setStats, {error, data:setStatsData}] = useMutation(SET_STATS);
+
+    const [formState, setFormState] = useState({
+        callsRecieved: '',
+        peopleServed: '',
+        reversals: '',
+
+    })
+
+    useEffect(() => {
+        if(data) {
+            setFormState({
+                callsRecieved: data.admin.callsRecieved,
+                peopleServed: data.admin.peopleServed,
+                reversals: data.admin.reversals,
+            });
+        }
+    }, [data]);
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    }
+    const statsSubmit = async (event) => {
+        event.preventDefault();
+        console.log('clicked the stat button')
+        try {
+            // This is here to check if something is put in the stat input and if there isn't anything, it won't update that stat as null
+            const variables = Object.fromEntries(
+                Object.entries(formState).filter(([key, value]) => value ).map(([key, value]) => [key, parseInt(value)])
+            );
+            await setStats({variables});
+            setFormState({
+                callsRecieved: formState.callsRecieved,
+                peopleServed: formState.peopleServed,
+                reversals: formState.reversals,
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    if(loading) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <form 
+            className='col-3 p-3'
+            onChange={handleChange}>
+            <div className="input-group mb-3">
+                <span 
+                    className="input-group-text" 
+                    id="inputGroup-sizing-callsRecieved">
+                        Calls Recieved
+                </span>
+                <input 
+                    name='callsRecieved' 
+                    type="number" 
+                    className="form-control" 
+                    aria-label="Sizing example input" 
+                    aria-describedby="inputGroup-sizing-default"
+                    value={formState.callsRecieved}
+                    onChange={handleChange} 
+                />
+            </div>
+            <div className="input-group mb-3">
+                <span 
+                    className="input-group-text" 
+                    id="inputGroup-sizing-peopleServed">
+                        People Served
+                </span>
+                <input 
+                    name='peopleServed' 
+                    type="number" 
+                    className="form-control" 
+                    aria-label="Sizing example input" 
+                    aria-describedby="inputGroup-sizing-default" 
+                    value={formState.peopleServed}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="input-group mb-3">
+                <span 
+                    className="input-group-text" 
+                    id="inputGroup-sizing-reversals">
+                        Reversals
+                </span>
+                <input 
+                    name='reversals' 
+                    type="number" 
+                    className="form-control" 
+                    aria-label="Sizing example input" 
+                    aria-describedby="inputGroup-sizing-default" 
+                    value={formState.reversals}
+                    onChange={handleChange}
+                />
+            </div> 
+            <button 
+                type='submit' 
+                className='primary' 
+                onClick={statsSubmit}>
+                    Update Stats
+            </button>
+        </form>
+    )
+
+}
--- client/src/pages/AdminPortal/index.jsx
+++ client/src/pages/AdminPortal/index.jsx
@@ -1,17 +1,17 @@
 import Auth from '../../utils/auth';
-import React from 'react';
-import { useState } from 'react';
-import { QUERY_ME } from '../../utils/queries';
-import { SET_STATS } from '../../utils/mutations';
+// import React from 'react';
+// import { useState } from 'react';
+// import { QUERY_ME } from '../../utils/queries';
+// import { SET_STATS } from '../../utils/mutations';
+// import { useMutation, useQuery } from '@apollo/client';
+import ChangeStatsForm from '../../components/adminComponents/statsForm'
 
-import { useMutation, useQuery } from '@apollo/client';
 
-export default function changeStats() {
 
-    const { loading, data: meData} = useQuery(QUERY_ME);
-    const userEmail = meData?.me?.email;
+export default function adminPortal() {
 
-    const [setStats, {error, data:setStatsData}] = useMutation(SET_STATS);
+    // const { loading, data: meData} = useQuery(QUERY_ME);
+    // const userEmail = meData?.me?.email;
     
     // handles the logout
     const isLoggedIn = Auth.loggedIn();
@@ -19,99 +19,14 @@ export default function changeStats() {
         Auth.logout();
     }
 
-    // handles the status update
-    const [formState, setFormState] = useState({
-        callsRecieved: '',
-        peopleServed: '',
-        reversals: '',
-
-    })
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        setFormState({
-            ...formState,
-            [name]: value
-        });
-    }
-    const statsSubmit = async (event) => {
-        event.preventDefault();
-        console.log('clicked the stat button')
-        try {
-            // This is here to check if something is put in the stat input and if there isn't anything, it won't update that stat as null
-            const variables = Object.fromEntries(
-                Object.entries(formState).filter(([key, value]) => value ).map(([key, value]) => [key, parseInt(value)])
-            );
-            await setStats({variables});
-            setFormState({
-                callsRecieved: '',
-                peopleServed: '',
-                reversals: '',
-            })
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
     return (
         <>
             {isLoggedIn ? (
                 <>
-                <h1>Hey there! {userEmail}</h1>
+                <h1>The Admin Portal!</h1>
+                <ChangeStatsForm />
 
-                <form 
-                    className='col-7 p-3'
-                    onChange={handleChange}>
-                    <div className="input-group mb-3">
-                        <span 
-                            className="input-group-text" 
-                            id="inputGroup-sizing-callsRecieved">
-                                Calls Recieved
-                        </span>
-                        <input 
-                            name='callsRecieved' 
-                            type="number" 
-                            className="form-control" 
-                            aria-label="Sizing example input" 
-                            aria-describedby="inputGroup-sizing-default" 
-                        />
-                    </div>
-                    <div className="input-group mb-3">
-                        <span 
-                            className="input-group-text" 
-                            id="inputGroup-sizing-peopleServed">
-                                People Served
-                        </span>
-                        <input 
-                            name='peopleServed' 
-                            type="number" 
-                            className="form-control" 
-                            aria-label="Sizing example input" 
-                            aria-describedby="inputGroup-sizing-default" 
-                        />
-                    </div>
-                    <div className="input-group mb-3">
-                        <span 
-                            className="input-group-text" 
-                            id="inputGroup-sizing-reversals">
-                                Reversals
-                        </span>
-                        <input 
-                            name='reversals' 
-                            type="number" 
-                            className="form-control" 
-                            aria-label="Sizing example input" 
-                            aria-describedby="inputGroup-sizing-default" 
-                        />
-                    </div> 
-
-                    {/* create a statsOnClick for this button */}
-                    <button 
-                        type='submit' 
-                        className='primary' 
-                        onClick={statsSubmit}>
-                            Update Stats
-                    </button>
-                </form>
+                {/* This is the logout button */}
                 <button 
                     className='primary m-3' 
                     onClick={logoutOnClick}>
