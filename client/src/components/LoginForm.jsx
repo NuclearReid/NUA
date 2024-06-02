dfd6482c67dd72d9d897019dd38ca25d9ac7e8e2
Reid Back <reid.backcnmt@gmail.com>
2024-06-02 17:50:56 +1000

    Will be redirected from the login page to the admin portal if youre already logged in:

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -39,50 +39,54 @@ export default function Login() {
         });
      };
 
+     const isLoggedIn = Auth.loggedIn();
+
     return (
         <>
-        <div className='d-flex justify-content-center'>
-            <form onSubmit={handleFormSubmit} className='col-3 p-4 mx-auto'>
-            <h1 className='fs-2 text-center'> 
-                Login
-            </h1>
-                <div className="mb-3">
-                    <label htmlFor="exampleLoginInputEmail" className="form-label">
-                        Username
-                    </label>
-                    
-                    <input 
-                        type="username" 
-                        className="form-control" 
-                        id="lgoinEmailField" 
-                        aria-describedby="loginEmailHelp" 
-                        name='loginEmail'
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label 
-                        htmlFor="inputPassword1" 
-                        className="form-label">
-                            Password
-                    </label>
-                    <input 
-                        type="password" 
-                        className="form-control"
-                        name='loginPassword'
-                        id="exampleLoginInputPassword1"
-                        onChange={handleChange} 
-                    />
-                </div>
-                <div className='text-center'>
-                    <button 
-                        type="submit" 
-                        className="btn btn-primary">
-                            Submit
-                    </button>
-                </div>
-            </form>
-        </div>
+        {isLoggedIn ? (window.location.assign('/adminPortal')):(
+            <div className='d-flex justify-content-center'>
+                <form onSubmit={handleFormSubmit} className='col-3 p-4 mx-auto'>
+                <h1 className='fs-2 text-center'> 
+                    Login
+                </h1>
+                    <div className="mb-3">
+                        <label htmlFor="exampleLoginInputEmail" className="form-label">
+                            Username
+                        </label>
+                        
+                        <input 
+                            type="username" 
+                            className="form-control" 
+                            id="lgoinEmailField" 
+                            aria-describedby="loginEmailHelp" 
+                            name='loginEmail'
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label 
+                            htmlFor="inputPassword1" 
+                            className="form-label">
+                                Password
+                        </label>
+                        <input 
+                            type="password" 
+                            className="form-control"
+                            name='loginPassword'
+                            id="exampleLoginInputPassword1"
+                            onChange={handleChange} 
+                        />
+                    </div>
+                    <div className='text-center'>
+                        <button 
+                            type="submit" 
+                            className="btn btn-primary">
+                                Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        )}
         </>
     )
 }
