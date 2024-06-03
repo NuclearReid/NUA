e4e696922eee3fbb7b04c27281f2d6355646a449
Reid Back <reid.backcnmt@gmail.com>
2024-06-03 15:33:03 +1000

    made a protectedRoute in the main.jsx (can only see these pages if logged in) and moved where the navbar is

--- client/src/components/adminComponents/statsForm.jsx
+++ client/src/components/adminComponents/statsForm.jsx
@@ -57,65 +57,65 @@ export default function changeStatsForm() {
 
     return (
         <>
-        <form 
-            className='col-3 p-3'
-            onChange={handleChange}>
-            <h1> Change Stats </h1>
-            <div className="input-group mb-3">
-                <span 
-                    className="input-group-text" 
-                    id="inputGroup-sizing-callsRecieved">
-                        Calls Recieved
-                </span>
-                <input 
-                    name='callsRecieved' 
-                    type="number" 
-                    className="form-control" 
-                    aria-label="Sizing example input" 
-                    aria-describedby="inputGroup-sizing-default"
-                    value={formState.callsRecieved}
-                    onChange={handleChange} 
-                />
-            </div>
-            <div className="input-group mb-3">
-                <span 
-                    className="input-group-text" 
-                    id="inputGroup-sizing-peopleServed">
-                        People Served
-                </span>
-                <input 
-                    name='peopleServed' 
-                    type="number" 
-                    className="form-control" 
-                    aria-label="Sizing example input" 
-                    aria-describedby="inputGroup-sizing-default" 
-                    value={formState.peopleServed}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="input-group mb-3">
-                <span 
-                    className="input-group-text" 
-                    id="inputGroup-sizing-reversals">
-                        Reversals
-                </span>
-                <input 
-                    name='reversals' 
-                    type="number" 
-                    className="form-control" 
-                    aria-label="Sizing example input" 
-                    aria-describedby="inputGroup-sizing-default" 
-                    value={formState.reversals}
-                    onChange={handleChange}
-                />
-            </div> 
-            <button 
-                type='submit' 
-                className='primary' 
-                onClick={statsSubmit}>
-                    Update Stats
-            </button>
-        </form>
+            <form 
+                className='col-3 p-3'
+                onChange={handleChange}>
+                <h1> Change Stats </h1>
+                <div className="input-group mb-3">
+                    <span 
+                        className="input-group-text" 
+                        id="inputGroup-sizing-callsRecieved">
+                            Calls Recieved
+                    </span>
+                    <input 
+                        name='callsRecieved' 
+                        type="number" 
+                        className="form-control" 
+                        aria-label="Sizing example input" 
+                        aria-describedby="inputGroup-sizing-default"
+                        value={formState.callsRecieved}
+                        onChange={handleChange} 
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <span 
+                        className="input-group-text" 
+                        id="inputGroup-sizing-peopleServed">
+                            People Served
+                    </span>
+                    <input 
+                        name='peopleServed' 
+                        type="number" 
+                        className="form-control" 
+                        aria-label="Sizing example input" 
+                        aria-describedby="inputGroup-sizing-default" 
+                        value={formState.peopleServed}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <span 
+                        className="input-group-text" 
+                        id="inputGroup-sizing-reversals">
+                            Reversals
+                    </span>
+                    <input 
+                        name='reversals' 
+                        type="number" 
+                        className="form-control" 
+                        aria-label="Sizing example input" 
+                        aria-describedby="inputGroup-sizing-default" 
+                        value={formState.reversals}
+                        onChange={handleChange}
+                    />
+                </div> 
+                <button 
+                    type='submit' 
+                    className='btn btn-primary' 
+                    onClick={statsSubmit}>
+                        Update Stats
+                </button>
+            </form>
         </>
     )
 
