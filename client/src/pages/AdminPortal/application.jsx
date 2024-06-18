d59147914477246d3285d9828d03c556c3ddeb0c
Reid Back <reid.backcnmt@gmail.com>
2024-06-18 11:35:02 +1000

    made the application page look a bit better

--- client/src/pages/AdminPortal/application.jsx
+++ client/src/pages/AdminPortal/application.jsx
@@ -1,10 +1,7 @@
 import ApplicationCardDisplay from '../../components/adminComponents/ApplicationCardDisplay';
-import volunteerOperatorPic from '../../assets/volunteerOperator.png'
 export default function AdminApplication() {
-
     return (
         <>
-        <img src={volunteerOperatorPic} alt='volunteer operator image'/>
             <ApplicationCardDisplay />
         </>
     )
